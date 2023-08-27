665b8ce04d31adc3e0316d15e16ef10e44c0a5c5
yali <yusufali0147@hotmail.com>
2023-08-27 15:50:22 +0300

    modifedd main and added png

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         int k ,toplam=0,sayac=0;
-        double ort=1;
+        double ort;
+
         Scanner imp= new Scanner(System.in);
         System.out.print("Girdiğiniz sayıya kadar olan üçe ve dörde bölünen sayıların ortalması alınacaktır.\nLütfen Sayı Giriniz :" );
         k=imp.nextInt();
+
         for(int i=1;i<=k;i++){
             if (i%3==0 && i%4==0){
                 toplam+=i ;
